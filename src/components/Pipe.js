10cd2019de7b9b8b81bec208ed9ba84b445fc5c8
lucia <lucialabatvaghi@gmail.com>
2019-10-29 20:00:30 -0300

    6 dummies basicos funcionando

--- src/components/Pipe.js
+++ src/components/Pipe.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { DragSource } from 'react-dnd';
 import Cross from '../svg/cross.svg';
+import Vertical from '../svg/dummies/vertical.svg';
+import Horizontal from '../svg/dummies/horizontal.svg';
 import TopRight from '../svg/dummies/top-right.svg';
-import TopDown from '../svg/dummies/top-down.svg';
+import TopLeft from '../svg/dummies/top-left.svg';
+import BottomLeft from '../svg/dummies/bottom-left.svg';
+import BottomRight from '../svg/dummies/bottom-right.svg';
 
 
 class Pipe extends React.Component {
@@ -15,16 +19,40 @@ class Pipe extends React.Component {
                     <Cross width={this.props.size} height={this.props.size}></Cross>
                 </div>  
             )
-        } else if (type=="top-down"){
+        } else if (type=="vertical"){
             return connectDragSource(
                 <div>
-                    <TopDown width={this.props.size} height={this.props.size}></TopDown>
+                    <Vertical width={this.props.size} height={this.props.size}></Vertical>
+                </div>
+            )
+        } else if (type=="horizontal"){
+            return connectDragSource(
+                <div>
+                    <Horizontal width={this.props.size} height={this.props.size}></Horizontal>
                 </div>
             )
         } else if (type=="top-right"){
             return connectDragSource(
                 <div>
                     <TopRight width={this.props.size} height={this.props.size}></TopRight>
+                </div>
+            )
+        } else if (type=="top-left"){
+            return connectDragSource(
+                <div>
+                    <TopLeft width={this.props.size} height={this.props.size}></TopLeft>
+                </div>
+            )
+        } else if (type=="bottom-left"){
+            return connectDragSource(
+                <div>
+                    <BottomLeft width={this.props.size} height={this.props.size}></BottomLeft>
+                </div>
+            )
+        } else if (type=="bottom-right"){
+            return connectDragSource(
+                <div>
+                    <BottomRight width={this.props.size} height={this.props.size}></BottomRight>
                 </div>
             )
         }
--- src/components/Toolbox.js
+++ src/components/Toolbox.js
@@ -6,36 +6,61 @@ export default class Toolbox extends React.Component {
 
     render() {
         return (
-            <div style={{display:"inline-block", verticalAlign:"top"}}>
+            <div style={{ display: "inline-block", verticalAlign: "top" }}>
                 <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                <Card.Title>Toolbox</Card.Title>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <Button variant="outline-primary">
-                                    <Pipe type="cross" size="30px"></Pipe>
-                                </Button>
-                            </td>
-                            <td>
-                                <Button variant="outline-primary">
-                                    <Pipe type="top-right" size="30px"></Pipe>
-                                </Button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <Button variant="outline-primary">
-                                    <Pipe type="top-down" size="30px"></Pipe>
-                                </Button>
-                            </td>
-                            <td>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                </Card.Body>
+                    <Card.Body>
+                        <Card.Title>Toolbox</Card.Title>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <Button variant="outline-primary">
+                                            <Pipe type="cross" size="30px"></Pipe>
+                                        </Button>
+                                    </td>
+                                    <td>
+                        
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Button variant="outline-primary">
+                                            <Pipe type="vertical" size="30px"></Pipe>
+                                        </Button>
+                                    </td>
+                                    <td>
+                                        <Button variant="outline-primary">
+                                            <Pipe type="horizontal" size="30px"></Pipe>
+                                        </Button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Button variant="outline-primary">
+                                            <Pipe type="top-left" size="30px"></Pipe>
+                                        </Button>
+                                    </td>
+                                    <td>
+                                        <Button variant="outline-primary">
+                                            <Pipe type="top-right" size="30px"></Pipe>
+                                        </Button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Button variant="outline-primary">
+                                            <Pipe type="bottom-left" size="30px"></Pipe>
+                                        </Button>
+                                    </td>
+                                    <td>
+                                        <Button variant="outline-primary">
+                                            <Pipe type="bottom-right" size="30px"></Pipe>
+                                        </Button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </Card.Body>
                 </Card>
             </div>
         )
