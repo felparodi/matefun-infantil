1b3247e78fc1e92194f77a46b217c552bb5890f4
lucia <lucialabatvaghi@gmail.com>
2019-10-29 10:26:33 -0300

    drag andando

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import Toolbox from './toolbox/Toolbox';
 import Board from './board/Board';
+import { DndProvider } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
 
 const App = ({ title }) =>
   <div>
-    <Toolbox></Toolbox>
-    <Board></Board>
+    <DndProvider backend={HTML5Backend}>
+      <Toolbox></Toolbox>
+      <Board></Board>
+    </DndProvider>
   </div>;
 
 export default App;
