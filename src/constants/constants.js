5724d8d6ebf573c3089c61efb9348f52d3fae11b
lucia <lucialabatvaghi@gmail.com>
2019-12-02 19:32:44 -0300

    constants

---
+++ src/constants/constants.js
@@ -0,0 +1,16 @@
+
+export const DIRECTION= Object.freeze({
+    TOP: 'TOP',
+    BOTTOM: 'BOTTOM',
+    LEFT: 'LEFT',
+    RIGHT: 'RIGHT'
+})
+
+export const PIPE_TYPES= Object.freeze({
+    DUMMY: 'DUMMY',
+    FUNCTION: 'FUNCTION',
+    VALUE: 'VALUE',
+    END: 'END'    
+})
+
+export default { DIRECTION, PIPE_TYPES };
