832057e84f0c7a7402c351060f5de146d7c84314
Felipe Oktana <felipe@oktana.io>
2019-12-03 17:11:54 -0300

    Refactor direction pipe

--- src/status/funcPipe.js
+++ src/status/funcPipe.js
@@ -1,39 +1,22 @@
-import {Pipe, FUNCTION } from './pipe'
+import { PIPE_TYPES, VALUES_TYPES } from '../constants/constants';
+import {Pipe} from './pipe'
 import {ValPipe} from './valPipe'
 
 export class FuncPipe extends ValPipe {
 
-    constructor(name, inType, outType) {
-       super(name);
-       this.pendingTypes = inType;
-       this.setInType(inType);
-       this.setOutType(outType);
+    constructor(name, inDirections, outDirections) {
+       super(name, outDirections);
+       this.setInDirection(inDirections);
+       this.setOutType(VALUES_TYPES.FUNCTION);
     }
 
     toString() {
         const arg = this.toStringArg();
-        return `${this.value}(${arg})`
+        return `${this.value}(${arg})`;
     }
 
     getType() {
-        return FUNCTION;
-    }
-
-    matchToIn(p) {
-        const thisInType = this.pendingTypes;
-        const otherOutType = p.getOutType();
-        if (thisInType.length >= otherOutType.length ) {
-            for (let i = 0; i < otherOutType.length; i++) {
-                if(thisInType[i] !== otherOutType[i]) return false
-            }
-            return true;
-        }
-        return false;
-    }
-
-    joinIn(p, dir) {
-       super.joinIn(p);
-       this.pendingTypes = this.pendingTypes.splice(p.getOutType().length);
+        return PIPE_TYPES.FUNCTION;
     }
 
 }
--- src/status/matrix.test.js
+++ src/status/matrix.test.js
@@ -1,64 +1,42 @@
 import 'jasmine';
-import { MatrixPipe, LEFT, RIGHT, DOWN, UP } from './matrix'
-import { ValPipe, TYPE_NUMBER, TYPE_BOOLEAN, TYPE_STRING } from './valPipe'
+import { DIRECTION } from '../constants/constants';
+import { MatrixPipe } from './matrix'
+import { ValPipe } from './valPipe'
 import { DummyPipe } from './dummyPipe'
 import { FuncPipe } from './funcPipe'
 import { EndPipe } from './endPipe'
 
-describe("MatrixPipe class test", ()=>{
+describe("MatrixPipe class test", () => {
     it("create single value matrix", () => { 
        const m = new MatrixPipe(5,5)
-       m.addPipe(2,2, new ValPipe(3))
-       m.addPipe(2,3, new EndPipe())
-       m.join(2,2, true, [RIGHT])
-       expect(m.processFunction()).toEqual("3 : <NUMBER>")
+       m.addPipe(2,2, new ValPipe(3, DIRECTION.RIGHT))
+       m.addPipe(2,3, new EndPipe(DIRECTION.LEFT));
+       let p = m.value(2,3);
+       expect(m.processFunction()).toEqual("3")
     })
     it("create single value using dummy pipe matrix", () => { 
         const m = new MatrixPipe(5,5)
-        m.addPipe(2,0, new ValPipe(3))
-        m.addPipe(2,1, new DummyPipe())
-        m.addPipe(2,2, new DummyPipe())
-        m.addPipe(2,3, new EndPipe())
-        m.join(2,0, true, [RIGHT])
-        m.join(2,1, true, [RIGHT])
-        m.join(2,2, true, [RIGHT])
-        expect(m.processFunction()).toEqual("3 : <NUMBER>")
+        m.addPipe(2,0, new ValPipe(3, DIRECTION.RIGHT))
+        m.addPipe(2,1, new DummyPipe(DIRECTION.LEFT, DIRECTION.RIGHT))
+        m.addPipe(2,2, new DummyPipe(DIRECTION.LEFT, DIRECTION.RIGHT))
+        m.addPipe(2,3, new EndPipe(DIRECTION.LEFT))
+        expect(m.processFunction()).toEqual("3")
      })
      it("create single value using function pipe matrix", () => { 
         const m = new MatrixPipe(5,5)
-        m.addPipe(2,0, new ValPipe(3))
-        m.addPipe(2,1, new FuncPipe('atoi',[TYPE_NUMBER], [TYPE_STRING]))
-        m.addPipe(2,2, new DummyPipe())
-        m.addPipe(2,3, new EndPipe())
-        m.join(2,0, true, [RIGHT])
-        m.join(2,1, true, [RIGHT])
-        m.join(2,2, true, [RIGHT])
-        expect(m.processFunction()).toEqual("atoi(3) : <STRING>")
+        m.addPipe(2,0, new ValPipe(3, DIRECTION.RIGHT))
+        m.addPipe(2,1, new FuncPipe('atoi',[DIRECTION.LEFT], [DIRECTION.RIGHT]))
+        m.addPipe(2,2, new DummyPipe(DIRECTION.LEFT, DIRECTION.RIGHT))
+        m.addPipe(2,3, new EndPipe(DIRECTION.LEFT))
+        expect(m.processFunction()).toEqual("atoi(3)")
      })
-     it("create single value using function pipe matrix", () => { 
+     it("create single value using function double arg pipe matrix", () => { 
         const m = new MatrixPipe(5,5)
-        m.addPipe(2,0, new ValPipe(3))
-        m.addPipe(1,1, new ValPipe(2))
-        m.addPipe(2,1, new FuncPipe('add',[TYPE_NUMBER, TYPE_NUMBER], [TYPE_NUMBER]))
-        m.addPipe(2,2, new DummyPipe())
-        m.addPipe(2,3, new EndPipe())
-        m.join(2,0, true, [RIGHT])
-        m.join(1,1, true, [DOWN])
-        m.join(2,1, true, [RIGHT])
-        m.join(2,2, true, [RIGHT])
-        expect(m.processFunction()).toEqual("add(3, 2) : <NUMBER>")
-     })
-     it("create single value using function pipe matrix", () => { 
-        const m = new MatrixPipe(5,5)
-        m.addPipe(2,0, new ValPipe(3))
-        m.addPipe(1,1, new ValPipe(2))
-        m.addPipe(2,1, new FuncPipe('duplicate',[TYPE_NUMBER, TYPE_NUMBER], [TYPE_NUMBER, TYPE_NUMBER]))
-        m.addPipe(2,2, new FuncPipe('add',[TYPE_NUMBER, TYPE_NUMBER], [TYPE_NUMBER]))
-        m.addPipe(2,3, new EndPipe())
-        m.join(2,0, true, [RIGHT])
-        m.join(1,1, true, [DOWN])
-        m.join(2,1, true, [RIGHT])
-        m.join(2,2, true, [RIGHT])
-        expect(m.processFunction()).toEqual("add(duplicate(3, 2)) : <NUMBER>")
+        m.addPipe(2,0, new ValPipe(3, DIRECTION.RIGHT))
+        m.addPipe(1,1, new ValPipe(2, DIRECTION.BOTTOM))
+        m.addPipe(2,1, new FuncPipe('add', [DIRECTION.LEFT, DIRECTION.TOP], [DIRECTION.RIGHT]))
+        m.addPipe(2,2, new DummyPipe(DIRECTION.LEFT, DIRECTION.RIGHT))
+        m.addPipe(2,3, new EndPipe(DIRECTION.LEFT))
+        expect(m.processFunction()).toEqual("add(3, 2)")
      })
 });
