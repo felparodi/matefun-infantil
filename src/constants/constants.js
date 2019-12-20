219e13e4e55d53af95b01d6a894f716f00460297
Felipe Oktana <felipe@oktana.io>
2019-12-20 11:05:39 -0300

    Ajustar tamanio de la grilla

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -40,8 +40,8 @@ export const ERROR = Object.freeze({
 
 export default { DIRECTION, PIPE_TYPES };
 
-export const BOARD_ROWS= 5;
-export const BOARD_COLS= 5;
+export const BOARD_ROWS= 8;
+export const BOARD_COLS= 14;
 
 export const CELL_SIZE=3;
 
