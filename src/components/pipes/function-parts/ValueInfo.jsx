9fa970d48efc21c5b9eb4c39e022b8d89b591285
Felipe Parodi <felipe.parodi91@gmail.com>
2020-03-16 19:20:19 -0300

    Color Icon

--- src/components/pipes/function-parts/ValueInfo.jsx
+++ src/components/pipes/function-parts/ValueInfo.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import {VALUES_TYPES, } from '../../../constants/constants';
 import TextIcon from './TextIcon';
+import Crallon from '../../../icons/crallon.svg';
 
+function testToColor(text) {
+    switch(text) {
+        case "Rojo":
+            return 'red';
+        case "Verde":
+            return 'green'
+        case "Azul":
+            return 'blue';
+        case "Negro":
+            return 'black';
+        default:
+            return text;
+    }
+}
 //TODO Agrear Mas Variantes para COLOR u otros
 //TODO dobleClick
 const ValueInfo = ({onClick, text, type}) => {
-    return <TextIcon onClick={onClick} text={text}/>;
+    switch(type) {
+        case VALUES_TYPES.COLOR:
+            return <Crallon onClick={onClick} style={{fill: testToColor(text)}}/>;
+        default:
+            return <TextIcon onClick={onClick} text={text}/>;
+    }
+    
 }
 
 export default ValueInfo;
