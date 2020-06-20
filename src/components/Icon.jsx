6b8041ba1664979f420d2d59f22321b7ed7765be
Felipe Parodi <felipe.parodi91@gmail.com>
2020-06-20 18:18:11 -0300

    Fix List Icon

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -181,10 +181,10 @@ function getCustomIcon(icon) {
         case icons.LIST:
             return (
                 <g>
-                    <circle cx="20" cy="5" r="1" stroke="currentColor" fill="red" strokeWidth="6"/>
-                    <circle cx="20" cy="15" r="1" stroke="currentColor" fill="red" strokeWidth="6"/>
-                    <circle cx="20" cy="25" r="1" stroke="currentColor" fill="red" strokeWidth="6"/>
-                    <circle cx="20" cy="35" r="1" stroke="currentColor" fill="red" strokeWidth="6"/>
+                    <circle cx="20" cy="5" r="1" stroke="currentColor" fill="currentColor" strokeWidth="6"/>
+                    <circle cx="20" cy="15" r="1" stroke="currentColor" fill="currentColor" strokeWidth="6"/>
+                    <circle cx="20" cy="25" r="1" stroke="currentColor" fill="currentColor" strokeWidth="6"/>
+                    <circle cx="20" cy="35" r="1" stroke="currentColor" fill="currentColor" strokeWidth="6"/>
                 </g>
             );
         default:    
