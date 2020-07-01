7720fa952e1b4deef7a82ce3cbfcfb917d529895
Lucia <lucialabatvaghi@gmail.com>
2020-07-01 18:39:19 -0300

    agrego mensajes de error

--- src/i18nextEsp.js
+++ src/i18nextEsp.js
@@ -2,11 +2,13 @@ export default {
     'no-connected': 'No está conectada a ninguna otra tubería',
     'no-connected-LEFT': 'No está conectada a otra tubería por la izquierda',
     'no-connected-TOP': 'No está conectada a otra tubería por arriba',
-    'no-connected-RIGHT': 'No está conectada a otra tubería por la dercha',
+    'no-connected-RIGHT': 'No está conectada a otra tubería por la derecha',
     'no-connected-BOTTOM': 'No está conectada a otra tubería por abajo',
     'no-match-type': 'Los tipos de las tuberías que se están uniendo no concuerdan',
     'loop': 'Hay un ciclo entre las tuberías',
     'no-next-func': 'No puede haber una funcion despues de un SI',
     'no-valid-type-assign': 'No es un valor válido para la asignación por el tipo',
     'no-boolean': 'A esta tubería no se le puede asignar un valor de tipo Verdadero o Falso',
+    'more-than-tree-var-pipes': 'Hay más de tres variables',
+    'more-than-on-end-pipe': 'Hay más de un resultado'
 }
